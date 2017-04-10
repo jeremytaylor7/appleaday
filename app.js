d6cb4254b9d15f4f213f11b53aee8404fe7534a4
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-04-10 14:03:05 -0700

    created params object in api function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//api key: IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j
+
+var baseURL = 'https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
+var api = 'https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
 //state
 
 var state = {
@@ -8,6 +12,19 @@ var state = {
 }
 
 //state functions
+
+function getFoodData(srch, callback) {
+    console.log('hello world');
+    var params = {
+        api_key: api,
+        It: 'f',
+        start: 0,
+        max: 20,
+        format: 'JSON'
+
+    }
+    $.getJSON(baseURL, params, callback);
+}
 
 //render
 
