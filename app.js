22aa6cabb182e965b1a8a70668197a14ddeb3eb0
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-04-10 14:35:44 -0700

    worked on render function getapi function working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //api key: IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j
 
 var baseURL = 'https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
-var api = 'https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
+var api = 'IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
 //state
 
 var state = {
@@ -17,16 +17,19 @@ function getFoodData(srch, callback) {
     console.log('hello world');
     var params = {
         api_key: api,
-        It: 'f',
-        start: 0,
-        max: 20,
-        format: 'JSON'
-
     }
     $.getJSON(baseURL, params, callback);
 }
 
 //render
+
+function renderFoodData(data) {
+
+    data.list.item.map(function (i) {
+        $('body').html('<p>' + i + '</p>');
+    })
+
+}
 
 
 //callbacks
