51c65131e22de28a5488503c6e6e3bce4c1c6b50
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-04-11 14:03:02 -0700

    added symptoms and fixed api correctly pulling data

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 //api key: IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j
 
-var baseURL = 'https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
+var baseURL = 'https://api.nal.usda.gov/ndb/nutrients/?'
 var api = 'IxsqmsuTjedK9mqAwWxyW2Kn1EYBbi4uTwy4wQ7j'
 //state
+/* food id's
+Vitamin C: 401
+
+*/
+
 
 var state = {
     symptoms: [
-        { type: 'allergies', remedies: 'vitamin C' },
-
+        { type: 'allergies', remedies: '401' },
+        { type: 'bad eyesight', remedies: '401' },
+        { type: 'headache', remedies: '401' },
+        { type: 'brain fog', remedies: '401' },
+        { type: 'sore throat', remedies: '401' },
+        { type: 'asthma', remedies: '401' },
+        { type: 'anxiety', remedies: '401' },
+        { type: 'stomach ache', remedies: '401' },
+        { type: 'depression', remedies: '401' },
+        { type: 'insomnia', remedies: '401' },
     ]
 }
 
 //state functions
 
-function getFoodData(srch, callback) {
+function getFoodData(srch, callback, id) {
     console.log('hello world');
     var params = {
+        format: 'JSON',
         api_key: api,
+        nutrients: srch,
+        sort: 'c',
+        subset: '1',
+        measureby: 'g'
+
     }
     $.getJSON(baseURL, params, callback);
 }
@@ -25,9 +44,7 @@ function getFoodData(srch, callback) {
 
 function renderFoodData(data) {
 
-    data.list.item.map(function (i) {
-        $('body').html('<p>' + i + '</p>');
-    })
+    console.log(data);
 
 }
 
