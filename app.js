2ef2b4ee8b7e0ded3d0c0155acdfaa3e6042c945
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-04-11 19:18:39 -0700

    topTen function pulling data correctly and adding top ten foods

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ function getFoodData(srch, callback, id) {
         nutrients: srch,
         sort: 'c',
         subset: '1',
-        measureby: 'g'
+        measureby: 'g',
+        max: 100,
 
     }
     $.getJSON(baseURL, params, callback);
@@ -45,17 +46,29 @@ function getFoodData(srch, callback, id) {
 
 function getTopTen(n) {
 
-    var foodArray = n.report.foods;
-    for (var i = 0; i <= foodArray.length; i++)
-        if (foodArray[i].nutrients[0].gm > 20) {
-            for (var i = 0; i <= 10; i++) {
-                foodArray.map(function (item) {
-                    state.foodSearch.name.push(item.name);
-                    state.foodSearch.name.push(item.nutrients[0].gm);
-                })
-            }
-        };
+    var theFoodArray = n.report.foods;
+
+    var foodList = state.foodSearch.name;
+    var ntrList = state.foodSearch.ntrValue;
+    theFoodArray.map(function (item) {
+        var ntr = item.nutrients[0].gm;
+        var foodName = item.name;
+        if (item.nutrients[0].gm > 20) {
+            foodList.push(foodName);
+            ntrList.push(ntr);
+            ntrList.sort(function (a, b) {
+                return b - a;
+            })
+        }
+
+    })
+    console.log('hello');
+    var slice1 = foodList.slice(0, 10);
+    var slice2 = ntrList.slice(0, 10);
+    state.foodSearch.name = slice1;
+    state.foodSearch.ntrValue = slice2;
 }
+
 function sortNutrients() {
 
 }
